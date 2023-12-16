c1c5f7875afdda4b6f49aa4b3ca6bff782d4f7ff
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-12-16 18:38:21 +0100

    update app add token

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./sidebar.css";
 import SidebarButton from "./sidebarButton";
 import { MdSpaceDashboard } from "react-icons/md";
@@ -6,16 +6,29 @@ import { FaGripfire, FaPlay } from "react-icons/fa";
 import { MdFavorite } from "react-icons/md";
 import { IoLibrary } from "react-icons/io5";
 import { FaSignOutAlt } from "react-icons/fa";
+import apiClient from "../../spotify";
 
 export const Sidebar = () => {
+  const [image, setImage] = useState(
+    "https://www.jeffersoncountypa.com/wp-content/uploads/2021/07/user.png"
+  );
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      console.log(response);
+      if (response.data.images && response.data.images.length > 0) {
+        setImage(response.data.images[0].url);
+      } else {
+        setImage(
+          "https://images.rawpixel.com/image_png_1100/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L3BmLWljb240LWppcjIwNjItcG9yLWwtam9iNzg4LnBuZw.png"
+        );
+        console.log("no image");
+      }
+    });
+  }, []);
   return (
     <div className="sidebar-container">
-      <img
-        src="https://images.pexels.com/photos/145707/pexels-photo-145707.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        className="profile-image"
-        alt="profile"
-      />
-      <div className="Container-btn">
+      <img src={image} className="profile-img" alt="profile" />
+      <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
         <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
